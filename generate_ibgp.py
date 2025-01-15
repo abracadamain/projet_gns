49502826b379c829934fd1a14d8a7b1f814ef4b8
zoejingwen <jshen9613@gmail.com>
2025-01-15 11:35:24 +0100

    test

--- generate_ibgp.py
+++ generate_ibgp.py
@@ -1,3 +1,4 @@
+import extraire_json
 def generate_ibgp_config(router: dict, as_data: dict) -> str:
     """
     Generates iBGP configuration for a router within an AS.
@@ -39,3 +40,43 @@ def generate_ibgp_config(router: dict, as_data: dict) -> str:
             })
 
     return ibgp_config
+'''
+as_data = {
+    "as_number": 100,
+    "routers": [
+        {
+            "hostname": "R11",
+            "interfaces": [
+                {"name": "GigabitEthernet1/0", "ip_address": "2001:db8:1::1", "network": "2001:db8:1::/64", "connected": "R12"},
+                {"name": "Loopback0", "ip_address": "2001:db8:100::1", "network": "2001:db8:100::1/128", "connected": None}
+            ]
+        },
+        {
+            "hostname": "R12",
+            "interfaces": [
+                {"name": "GigabitEthernet1/0", "ip_address": "2001:db8:1::2", "network": "2001:db8:1::/64", "connected": "R11"},
+                {"name": "GigabitEthernet2/0", "ip_address": "2001:db8:2::1", "network": "2001:db8:2::/64", "connected": "R13"},
+                {"name": "Loopback0", "ip_address": "2001:db8:100::2", "network": "2001:db8:100::2/128", "connected": None}
+            ]
+        },
+        {
+            "hostname": "R13",
+            "interfaces": [
+                {"name": "GigabitEthernet2/0", "ip_address": "2001:db8:2::2", "network": "2001:db8:2::/64", "connected": "R12"},
+                {"name": "Loopback0", "ip_address": "2001:db8:100::3", "network": "2001:db8:100::3/128", "connected": None}
+            ]
+        }
+    ],
+    "ibgp_links": [["R11", "R12"], ["R12", "R13"], ["R11", "R13"]]
+}
+router = {
+    "hostname": "R11",
+    "interfaces": [
+        {"name": "GigabitEthernet1/0", "ip_address": "2001:db8:1::1", "network": "2001:db8:1::/64", "connected": "R12"},
+        {"name": "Loopback0", "ip_address": "2001:db8:100::1", "network": "2001:db8:100::1/128", "connected": None}
+    ]
+}
+# 调用 generate_ibgp_config 函数
+ibgp_config = generate_ibgp_config(router, as_data)
+print(ibgp_config)
+'''
