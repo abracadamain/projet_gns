46e5d1414693edb7d79eec4af0ff8efd2f7782d7
abracadamain <paulineigue@gmail.com>
2025-01-27 17:22:56 +0100

    fonction qui genère la liste de toutes les adresses réseau

---
+++ gen_network.py
@@ -0,0 +1,20 @@
+import extraire_json
+import gen_ip
+
+def gen_address_network():
+    '''
+    Récupère toutes les adresses réseau
+    '''
+    data=extraire_json.read_intent_file("network_intents.json")
+    address_network = []
+    for ausys in data["network"]["autonomous_systems"]:
+        for router in ausys["routers"]:
+            dict_ip = gen_ip.allocate_ip_add_routeur("network_intents.json", router["hostname"])
+            for address in dict_ip:
+                if address != "Loopback0":
+                    add_net = dict_ip[address][:-5]
+                    if add_net not in address_network:
+                        address_network.append(add_net)
+    print(address_network)
+
+gen_address_network()
