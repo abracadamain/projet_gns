1e852ae958bb2f7b1df4153680f0bdc010e8d404
abracadamain <paulineigue@gmail.com>
2025-02-04 17:05:25 +0100

    suppression advertise network loopback

--- gen_network.py
+++ gen_network.py
@@ -21,19 +21,3 @@ def gen_address_network(as_number):
                                 if add_net not in address_network:
                                     address_network.append(add_net)
     return address_network
-
-def gen_address_loopback_network(as_number):
-    '''
-    Récupère toutes les adresses de loopback d'un AS
-    '''
-    data=extraire_json.read_intent_file("network_intents.json")
-    address_network = []
-    for ausys in data["network"]["autonomous_systems"]:
-        if ausys["as_number"] == as_number:
-            for router in ausys["routers"]:
-                dict_ip = gen_ip.allocate_ip_add_routeur("network_intents.json", router["hostname"])
-                for address in dict_ip:
-                    if address == "Loopback0":
-                        add_net = dict_ip[address]
-                        address_network.append(add_net)
-    return address_network
