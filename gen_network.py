2d7cf6f6fab104a25e6fd7cb986f70f5d69b9b28
abracadamain <paulineigue@gmail.com>
2025-02-05 13:00:00 +0100

    suppression advertise network lien physique

--- gen_network.py
+++ gen_network.py
@@ -21,22 +21,3 @@ def gen_address_network(as_number):
                                 if add_net not in address_network:
                                     address_network.append(add_net)
     return address_network
-
-def gen_address_network_direct_lien_direct(routeur_hostname) :
-    '''
-    Récupère toutes les adresses réseau en lien direct avec un routeur
-    '''
-    data=extraire_json.read_intent_file("network_intents.json")
-    network_direct = []
-    for ausys in data["network"]["autonomous_systems"]:
-        for router in ausys["routers"]:
-            if router["hostname"] == routeur_hostname:
-                dict_ip = gen_ip.allocate_ip_add_routeur("network_intents.json", router["hostname"])
-                for address in dict_ip:
-                    if address != "Loopback0":
-                        for interface in router["interfaces"]:
-                            if interface["name"] == address :
-                                add_net = dict_ip[address][:-5]
-                                if add_net not in network_direct:
-                                    network_direct.append(add_net)
-    return network_direct
